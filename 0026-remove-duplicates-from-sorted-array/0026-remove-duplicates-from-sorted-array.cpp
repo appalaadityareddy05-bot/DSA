4d14e3293d4f866263ae2482a3339123abcf8a9e
APPALA ADITYA REDDY <appalaadityareddy05@gmail.com>
2026-07-13 23:28:22 +0530

    Time: 0 ms (100%), Space: 22.7 MB (53.33%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = 0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[n] != nums[i]){
+                nums[++n] = nums[i];
+            }
+        }
+        return n+1;
+    }
+};
